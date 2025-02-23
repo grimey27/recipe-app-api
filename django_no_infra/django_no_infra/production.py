e3464ab4d711c6def6b04df1783586346c894a83
Jeyson Grimm <58368304+grimey27@users.noreply.github.com>
2025-02-23 07:35:46 +0000

    UPDATE 2

---
+++ django_no_infra/django_no_infra/production.py
@@ -0,0 +1,66 @@
+import os
+
+from .settings import *  # noqa
+from .settings import BASE_DIR
+
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+DEBUG = False
+
+# NEW OLD
+SECRET_KEY = os.environ['MY_SECRET_KEY']
+
+# WhiteNoise configuration
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # Add whitenoise middleware after the security middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['AZURE_POSTGRESQL_NAME'],
+        'HOST': os.environ['AZURE_POSTGRESQL_HOST'],
+        'USER': os.environ['AZURE_POSTGRESQL_USER'],
+        'PASSWORD': os.environ['AZURE_POSTGRESQL_PASSWORD'],
+    }
+}
+'''
+# INFO DEL VIDEO PARA TESTING
+CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": CONNECTION_STR['dbname'],
+        "HOST": CONNECTION_STR['host'],
+        "USER": CONNECTION_STR['user'],
+        "PASSWORD": CONNECTION_STR['password'],
+    }
+}
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
---
+++ django_no_infra/spce/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "spce"
